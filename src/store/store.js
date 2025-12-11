af62436ed0e2a1c7b1c1bfee802c70292d0a8230
SaiBalMondiale <mondialedev@gmail.com>
2025-12-11 20:05:03 +0530

    book slice and borrow slice created

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,16 @@ import {configureStore} from "@reduxjs/toolkit";
 import authResucer from "./slices/authSlice"
 import popupReduser from "./slices/popUpSlice"
 import userReducer from "./slices/userSlice"
+import bookReducer from "./slices/bookSlice"
+
+
+
 export const store = configureStore({
     reducer:{
         auth:authResucer,
         popup:popupReduser,
-        user:userReducer
+        user:userReducer,
+        book: bookReducer
     },
     
 })
